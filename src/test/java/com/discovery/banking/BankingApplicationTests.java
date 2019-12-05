f4423816bcd8d9e485b158a919388e5e924d0707
Joshua <Joshua.forssman@eoh.com>
2019-12-05 08:42:40 +0200

    Creation of project base

---
+++ src/test/java/com/discovery/banking/BankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discovery.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
